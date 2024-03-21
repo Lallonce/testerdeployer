75da91c995483201afbecfbd9cc1c5d427f5eb6a
Lesly Allonce <151698977+Lallonce@users.noreply.github.com>
2024-03-21 18:54:32 -0400

    edited database url

--- db/client.js
+++ db/client.js
@@ -2,6 +2,7 @@ const { Client } = require("pg")
 require("dotenv").config()
 
 let DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:5432/luigis_pizzeria"
+// console.log(DATABASE_URL)
 
 // console.log(DATABASE_URL)
 const client = new Client({ connectionString: DATABASE_URL, ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined, })
